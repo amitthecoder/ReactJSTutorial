9f8a414d7ee0bc56bc1c6a11fd16637accbaab42
Amit Upadhyay <65838692+amitthecoder@users.noreply.github.com>
2020-06-12 02:15:41 +0530

    Basic State and Set State information

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
 
-  return <div>HI there</div>;
-};
+  render() {
+    return (
+      <div>
+        latitude :{this.state.lat}
+        <br />
+        Error :{this.state.errorMessage}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
